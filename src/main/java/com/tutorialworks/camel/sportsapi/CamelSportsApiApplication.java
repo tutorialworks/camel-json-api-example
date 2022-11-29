d6ee93b2790285dd972b82e4abc75ac23ad8db5f
Tom Donohue <monodot@gmail.com>
2022-11-29 10:05:38 +0000

    Remove extraneous

--- src/main/java/com/tutorialworks/camel/sportsapi/CamelSportsApiApplication.java
+++ src/main/java/com/tutorialworks/camel/sportsapi/CamelSportsApiApplication.java
@@ -2,8 +2,6 @@ package com.tutorialworks.camel.sportsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class CamelSportsApiApplication {
